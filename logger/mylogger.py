b8ca37ca5f41d88759d4a7db944f0825c319d362
kacky355 <misakipovknightend@gmail.com>
2024-06-30 19:32:49 +0900

    bug fix get_my_logger

--- logger/mylogger.py
+++ logger/mylogger.py
@@ -2,7 +2,7 @@ from logging import getLogger,DEBUG, INFO,Formatter, config
 import os
 import json
 
-def get_my_logger(base_dir:str,filename:str='main.log', config_path=None):
+def get_my_logger(base_dir:str,logger_name:str=None, level=INFO, handlers:list=["consoleHandler", "fileHandler"], propagate=False, filename:str='main.log', config_path=None):
     log_dir = os.path.join(base_dir, 'logs')
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
@@ -14,12 +14,18 @@ def get_my_logger(base_dir:str,filename:str='main.log', config_path=None):
     else:
         log_conf=_get_default_config()
         log_conf['handlers']['fileHandler']['filename'] = logfile_dir
+    
+        
+    if logger_name:
+        log_conf['loggers'].pop('main_log')
+        log_conf = _set_new_logger(log_conf, logger_name, level, handlers, propagate)
+    else:
+        logger_name='main_log'
 
     config.dictConfig(log_conf)
-    
-    logger=getLogger(__name__)
+    logger=getLogger(logger_name)
     logger.info(f'logger has made. log_dir:{log_dir}')
-
+        
     return logger
 
 def _get_default_config():
@@ -48,7 +54,7 @@ def _get_default_config():
         },
 
         "loggers": {
-            "__main__": {
+            "main_log": {
                 "level": "DEBUG",
                 "handlers": ["consoleHandler", "fileHandler"],
                 "propagate": False
@@ -61,3 +67,13 @@ def _get_default_config():
     }
     
     return log_conf
+
+def _set_new_logger(log_conf:dict, logger_name:str, level, handlers:list, propagate:bool):
+    conf = log_conf
+    new_logger = {
+        "level" : level,
+        "handlers" : handlers,
+        "propagate" : propagate
+    }
+    conf['loggers'][logger_name] = new_logger
+    return conf
