45970be138a3a65af4bf6307f82d3ca8588b7bd6
kacky355 <misakipovknightend@gmail.com>
2024-06-30 17:39:32 +0900

    update get_my_logger

--- logger/mylogger.py
+++ logger/mylogger.py
@@ -2,15 +2,18 @@ from logging import getLogger,DEBUG, INFO,Formatter, config
 import os
 import json
 
-def get_my_logger(base_dir:str,filename:str='main.log'):
+def get_my_logger(base_dir:str,filename:str='main.log', config_path=None):
     log_dir = os.path.join(base_dir, 'logs')
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
     logfile_dir=os.path.join(log_dir, filename)
     
-    with open('logger/log_config.json', 'r') as f:
-        log_conf = json.load(f)
-    log_conf['handlers']['fileHandler']['filename'] = logfile_dir
+    if config_path:
+        with open(config_path, 'r') as f:
+            log_conf = json.load(f)
+    else:
+        log_conf=_get_default_config()
+        log_conf['handlers']['fileHandler']['filename'] = logfile_dir
 
     config.dictConfig(log_conf)
     
@@ -18,3 +21,43 @@ def get_my_logger(base_dir:str,filename:str='main.log'):
     logger.info(f'logger has made. log_dir:{log_dir}')
 
     return logger
+
+def _get_default_config():
+    log_conf = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "%(asctime)s %(name)s:%(lineno)s %(funcName)s [%(levelname)s]: %(message)s"
+            }
+        },
+
+        "handlers": {
+            "consoleHandler": {
+                "class": "logging.StreamHandler",
+                "level": "DEBUG",
+                "formatter": "simple",
+                "stream": "ext://sys.stdout"
+            },
+            "fileHandler": {
+                "class": "logging.FileHandler",
+                "level": "INFO",
+                "formatter": "simple",
+                "filename": ""
+            }
+        },
+
+        "loggers": {
+            "__main__": {
+                "level": "DEBUG",
+                "handlers": ["consoleHandler", "fileHandler"],
+                "propagate": False
+            }
+        },
+
+        "root": {
+            "level": "INFO"
+        }
+    }
+    
+    return log_conf
